7027a307a73700fe320ee957909eae0ffbe5e209
Jamille <santosjamille998@gmail.com>
2023-11-12 21:24:37 -0300

    feat: adding valid and invalid letters

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -1,34 +1,80 @@
+import { FormEvent, useState, useRef } from "react";
 import "./Game.css";
 
 interface GameProps {
-  verifyLetter: () => void;
+  verifyLetter: (letter: string) => void;
+  pickedWord: string;
+  pickedCategory: string;
+  letters: string[];
+  guessedLetters: string[];
+  wrongLetters: string[];
+  guesses: number;
+  score: number;
 }
 
-function Game({ verifyLetter }: GameProps) {
+function Game({
+  verifyLetter,
+  pickedWord,
+  pickedCategory,
+  letters,
+  guessedLetters,
+  wrongLetters,
+  guesses,
+  score,
+}: GameProps) {
+  const [letter, setLetter] = useState<string>("");
+  const letterInputRef = useRef<HTMLInputElement>(null);
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+
+    verifyLetter(letter);
+    setLetter("");
+    if (letterInputRef.current) {
+      letterInputRef.current.focus();
+    }
+  };
   return (
     <div className="game">
       <p className="points">
-        <span>Punctuation: 000</span>
+        <span>Punctuation: {score}</span>
       </p>
       <h1>Guess the word:</h1>
       <h3 className="tip">
-        Tip about the word <span>tip...</span>
+        Tip about the word <span>{pickedCategory}</span>
       </h3>
+      <p>You still have {guesses} attempts</p>
       <div className="wordContainer">
-        <span className="letter">A</span>
-        <span className="blankSquare"></span>
+        {letters.map((letter, index) =>
+          guessedLetters.includes(letter) ? (
+            <span key={index} className="letter">
+              {letter}
+            </span>
+          ) : (
+            <span key={index} className="blankSquare"></span>
+          )
+        )}
       </div>
       <div className="letterContainer">
         <p>Try to guess a letter of the word</p>
-        <form>
-          <input type="text" name="letter" maxLength={1} required />
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="letter"
+            maxLength={1}
+            required
+            onChange={(text) => setLetter(text.target.value)}
+            value={letter}
+            ref={letterInputRef}
+          />
           <button>To play</button>
         </form>
       </div>
       <div className="wrongLettersContainer">
         <p>Letters already used</p>
-        <span>a, </span>
-        <span>b, </span>
+        {wrongLetters.map((letters, index) => (
+          <span key={index}>{letters}, </span>
+        ))}
       </div>
     </div>
   );
